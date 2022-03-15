7d497a05a0693c00c9154ecfd0b164d0daa489d1
John Mai <johnhuymai@gmail.com>
2022-03-15 12:03:18 -0500

    Added player rotation and rotation constraints
    I added a Flip() method in the PlayerBehavior class to allow the player sprite to change directions. This will help with implementing the shooting function for the player.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,8 +1,9 @@
 ﻿// Author:          Scott Krabbenhoft
 // Created on:      03/13/2022
 
-// Last edited by:  Scott Krabbenhoft
-// Last edited on:  03/13/2022
+// Last edited by:  John Mai
+// Last edited on:  03/15/2022
+// Edits made:      Added flip script to allow player sprite to face different directions
 
 // Description:     Handles player behaviors, which is just movement for now
 
@@ -16,11 +17,12 @@ public class PlayerBehavior : MonoBehaviour
     [SerializeField] float speed = 4f;
     [SerializeField] float jumpSpeed = 4f;
 
-    Rigidbody2D rigidBody;
-    CapsuleCollider2D capsule;
+    Rigidbody2D rigidBody = null;
+    CapsuleCollider2D capsule = null;
     float radius;
     bool grounded = false;
     bool doubleJump = false;
+    bool facingRight = true;
     
     void Start()
     {
@@ -35,16 +37,17 @@ public class PlayerBehavior : MonoBehaviour
         // checks for any collider directly under the player -- NOTE: this will include any enemies or objects that have colliders,
         //                                                            so collision masks may be necessary in the future
         if (Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.down, radius + 0.1f))
-            {
-                // sets grounded and double jump to true if the player is on a valid surface
-                grounded = true;
-                doubleJump = true;
-            }
-            else
-            {
-                // sets grounded to false if the player is not on a valid surface -- NOTE: double jump should not be edited here
-                grounded = false;
-            }
+        {
+            // sets grounded and double jump to true if the player is on a valid surface
+            grounded = true;
+            doubleJump = true;
+        }
+        else
+        {
+            // sets grounded to false if the player is not on a valid surface -- NOTE: double jump should not be edited here
+            grounded = false;
+        }
+
         // forces the player upwards when the jump button is hit, accounting for gravity
         if (Input.GetButtonDown("Jump") && (grounded || doubleJump))
         {
@@ -60,6 +63,27 @@ public class PlayerBehavior : MonoBehaviour
         rigidBody.position = transform.position + new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
         // zeroes out x velocity to prevent sliding errors
         rigidBody.velocity = new Vector3(0, rigidBody.velocity.y, 0);
-        
+
+        // If player moves left while facing right, flip the player to the left
+        if (Input.GetKeyDown(KeyCode.A) && facingRight)
+        {
+            Flip();
+        }
+        // If plyer moves right while facing left, flip player to right
+        else if (Input.GetKeyDown(KeyCode.D) && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        // Switch way the player is labelled as facing
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1, flipping them
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
