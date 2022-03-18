30873a5760751d3d05a28b2d80b3e69a17815da6
John Mai <johnhuymai@gmail.com>
2022-03-18 14:27:45 -0500

    Implemented UI for score
    Working score display in the upper left screen. Updates according to correct pick ups from enemies

---
+++ Assets/Scripts/DisplayScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour
+{
+    [SerializeField] PlayerBehavior player;
+    [SerializeField] Text scoreContainer;
+
+    private void Update()
+    {
+        scoreContainer.text = player.score.ToString();
+    }
+}
