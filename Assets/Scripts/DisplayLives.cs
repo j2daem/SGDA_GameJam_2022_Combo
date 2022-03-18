cd7a893b51ff8faaebb6c0687fbfa7fa7b2a9e73
John Mai <johnhuymai@gmail.com>
2022-03-18 13:16:20 -0500

    Implemented Lives UI
    Implemented UI text to keep track of player lives

--- Assets/Scripts/DisplayLives.cs
+++ Assets/Scripts/DisplayLives.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections;
+﻿// Author:          John Mai
+// Created on:      03/16/2022
+
+// Last edited by: John Mai
+// Last edited on:  03/18/2022
+// Last edits made: XXX
+
+// Description:     UI display for the life count
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class DisplayLives : MonoBehaviour
 {
     [SerializeField] PlayerBehavior player;
-    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] Text livesContainer;
 
-
+    private void Update()
+    {
+        livesContainer.text = player.lives.ToString();
+    }
 }
